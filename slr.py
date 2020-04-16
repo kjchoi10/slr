d3bae3ca8eecd1e7e2a1a2a065bc9c56d9b6d325
Kevin <kjchoi10@gmail.com>
2020-04-16 13:39:00 -0400

    updated with ma estiamtes

--- slr.py
+++ slr.py
@@ -47,16 +47,28 @@ def _slr(
    # merge seasonal_index with output_data
    output_data = pd.merge(input_data, seasonal_index, on=['period_number'])
 
-   
+   # apply seasonal index to input_exog
+
 
    return()
 
+ # model using statsmodel
+import statsmodels.api as sm
+from itertools import cycle
+import numpy as np
+import pandas as pd
+import math
 
-def fit_slr(df):
+def slr(df, forecast_length, n_season, trend_dampening):
   """
-  :params: pandas time series with seasonal index calculated
+  :params df: pandas time series with seasonal index calculated
+  :params forecast_length: integer for length forecast
+  :params n_season: type of season (i.e. 12 for monthly)
+  :params trend_damp: float number where 1 is default
   :return: model object, forecast
   """
+  # available seasons
+  n_k = len(df)/n_season
 
   # create year column
   df['month_number'] = [x.month for x in df['month']]
@@ -68,14 +80,44 @@ def fit_slr(df):
   n_season = 12
 
   # moving average
-  m_a = df['training_response'].rolling(window=n_season).mean()
+  m_a = df['training_response'].rolling(window=n_season).mean().dropna().tolist()
 
   # centered moving average
-  m_a_centered = m_a.rolling(window=2).mean().dropna().tolist()
+  #m_a_centered = m_a.rolling(window=2).mean().dropna().tolist()
+
+  # (need to add even or odd check)
   midpoint = (n_season/2)+1
 
+  # estimate remaining centered moving average
+  if(n_k.is_integer() == False):
+
+    # account for after a period
+    trailing = 1
+
+    # tail of m_a
+    m_a_remainder = ((math.ceil(n_k)-n_season)-n_total)+trailing
+
+    # total m_a
+    m_a_len = len(m_a) + m_a_remainder
+
+    # period for m_a once centered
+    period_ma = np.array(range(int(midpoint), int(midpoint)+len(m_a)))
+
+    # forecast period for m_a once centered
+    forecast_period_ma = np.array(range(int(midpoint)+len(m_a), n_total))
+
+    # returns tail m_a
+    m_a_res = moving_average_estimate(m_a, period_ma, forecast_period_ma)
+
+    # full m_a
+    m_a.extend(m_a_res)
+
+  elif(n_k.is_interger() == True):
+
+    m_a = m_a
+
   # add centered moving average starting at midpoint index
-  df.loc[df.index[int(midpoint):int(midpoint)+len(m_a_centered)], 'm_a_centered'] = m_a_centered
+  df.loc[df.index[int(midpoint):int(midpoint)+len(m_a)], 'm_a_centered'] = m_a
 
   # seasonal irregular value
   df['season_irregular'] = df['training_response']/df['m_a_centered']
@@ -83,9 +125,6 @@ def fit_slr(df):
   # extend the rest of the season with the latest m_a value / or for now we can just replace NAN values with 1 which is trend
   seasonal_index = df.groupby('month_number').agg({'season_irregular': 'mean'}).fillna(1).reset_index().rename({'season_irregular': 'seasonal_index'}, axis = 1)
   d = pd.merge(df, seasonal_index, on = ['month_number'])
-
-  # apply seasonal index to soh
-  d['coef'] = d['training_response'] * d['seasonal_index']
 
   # sort months
   d = d.sort_values(by='month')
@@ -105,26 +144,43 @@ def fit_slr(df):
   model = sm.OLS(y, X)
   res = model.fit()
 
+  # dampen the beta coefficent
+  res.params[1] = res.params[1]*trend_dampening
+
   # prediction input (period)
-  X_lately = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21, 22, 23, 24, 25, 26, 27]
+  forecast_period = np.array(range(len(df)+forecast_length))
+
   # intercept
-  constant = np.ones_like(X_lately)
+  forecast_constant = np.ones_like(forecast_period)
+
   # create prediction input
-  X_new = np.column_stack((constant, X_lately))
+  forecast_input = np.column_stack((forecast_constant, forecast_period))
+
   # list of forecasts including in-sample and out-sample
-  forecast_set = model.predict(res.params, X_new)
+  forecast_set = model.predict(res.params, forecast_input)
+
   # forecast dataframe
-  df_forecast = pd.DataFrame({'period': X_lately, 'forecast': forecast_set})
+  forecast_output = pd.DataFrame({'period': forecast_period, 'forecast': forecast_set})
 
   # Adjusted period for repeating 0-12 months
-  seq = cycle(range(13))
+  seq = cycle(range(n_season))
 
-  df_forecast['period_adj'] = [next(seq) for count in range(df_forecast.shape[0])]
+  forecast_output['period_adj'] = [next(seq) for count in range(forecast_output.shape[0])]
 
   # Right join seasonal index
-  df_forecast = df_forecast.merge(df[['seasonal_index', 'period']], left_on='period_adj', right_on='period', how='left')
+  forecast_output = forecast_output.merge(df[['seasonal_index', 'period']], left_on='period_adj', right_on='period', how='left')
 
-  # Forecast adjusted based on the period and seasonal index
-  df_forecast['forecast_adj'] = df_forecast['forecast'] * df_forecast['seasonal_index']
+   # Forecast adjusted based on the period and seasonal index
+  forecast_output['forecast_adj'] = forecast_output['forecast'] * forecast_output['seasonal_index']
 
-  return(model, df_forecast)
+  return(res, forecast_output, df)
+
+
+def moving_average_estimate(y, X, forecast_period):
+  X = sm.add_constant(X)
+  model = sm.OLS(y,X)
+  res = model.fit()
+  forecast_constant = np.ones_like(forecast_period)
+  forecast_input = np.column_stack((forecast_constant, forecast_period))
+  forecast_set = model.predict(res.params, forecast_input)
+  return(forecast_set)
